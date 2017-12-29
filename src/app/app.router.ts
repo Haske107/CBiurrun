66b12733141d1c028fd1f39e5d3b48d03f8ab741
Jeff <Haske107@mail.chapman.edu>
2017-12-28 23:22:59 -0800

    s

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,9 +4,7 @@ import {LandingComponent} from "./landing/landing.component";
 
 const   APP_ROUTES: Routes = [
     // Home Page Routes
-    {path: '', redirectTo: '/Landing', pathMatch: 'full'},
-    {path: 'Landing', component: LandingComponent},
-
+    {path: '', component: LandingComponent}
 ];
 
 export const AppRouter = RouterModule.forRoot(APP_ROUTES);
